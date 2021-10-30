01d15fb35370998b84541275f27aa7db2888d118
samuela <Samuelahsu@gmail.com>
2021-10-31 04:49:41 +0800

    新增登入註冊提示訊息 JS / HTML

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -8,7 +8,7 @@ $(function () {
     });
 
 
-    //前端姓名格式判斷
+    //前端姓名格式判斷 erName
     $('#name').blur(function (e) {
         e.preventDefault();
 
@@ -27,26 +27,25 @@ $(function () {
         if (name == "") {
             $('#name').addClass('border border-1 border border-danger');
             $('#submitButton').addClass('disabled');
-            // $('#message').text('姓名格式錯誤');
-            $('#erName').html('<span style="color: red;">姓名欄不可為空</span>');
-            // alert('姓名');
+            $('#message').text('姓名格式錯誤');
+            $('.erName').text('姓名欄不可為空');
+
         } else if (name.trim().length < 2) {
 
             $('#name').addClass('border border-1 border border-danger');
             $('#submitButton').addClass('disabled');
-            // $('#message').text('姓名格式錯誤');
-            $('#erName').html('<span style="color: red;">姓名格式錯誤</span>');
+            $('#message').text('姓名格式錯誤');
+            $('.erName').text('姓名格式須大於等於2個字');
 
         }else{
             $('#name').removeClass('border border-1 border border-danger');
             $('#submitButton').removeClass('disabled');
             $('#message').empty();
-            $('#erName').remove();
-        
+            $('.erName').empty();
         }
     });
 
-    //前端暱稱格式判斷
+    //前端暱稱格式判斷 erNickName
     $('#nickname').blur(function (e) {
         e.preventDefault();
 
@@ -56,15 +55,17 @@ $(function () {
             $('#nickname').addClass('border border-1 border border-danger');
             $('#submitButton').addClass('disabled');
             $('#message').text('暱稱格式錯誤');
+            $('.erNickName').text('暱稱格式錯誤');
         } else {
             $('#nickname').removeClass('border border-1 border border-danger');
             $('#submitButton').removeClass('disabled');
             $('#message').empty();
+            $('.erNickName').empty();
         }
 
     });
 
-    //前端信箱格式判斷
+    //前端信箱格式判斷 erEmail
     $('#email').blur(function (e) {
         e.preventDefault();
 
@@ -75,15 +76,17 @@ $(function () {
             $('#email').addClass('border border-1 border border-danger');
             $('#submitButton').addClass('disabled');
             $('#message').text('信箱格式錯誤');
+            $('.erEmail').text('信箱格式錯誤');
         } else {
             $('#email').removeClass('border border-1 border border-danger');
             $('#submitButton').removeClass('disabled');
             $('#message').empty();
+            $('.erEmail').empty();
         }
 
     });
 
-    //帳號確認按鈕註冊Ajax請求
+    //帳號確認按鈕註冊Ajax請求 erUsername
     $('#username').blur(function (e) {
         //取消原按鈕功能
         e.preventDefault();
@@ -96,11 +99,13 @@ $(function () {
             $('#username').addClass('border border-1 border border-danger');
             $('#submitButton').addClass('disabled');
             $('#message').text('帳號格式錯誤');
+            $('.erUsername').text('帳號格式錯誤');
             return;
         } else {
             $('#username').removeClass('border border-1 border border-danger');
             $('#submitButton').removeClass('disabled');
             $('#message').empty();
+            $('.erUsername').empty();
         }
         //創建物件
         let data = {};
@@ -115,17 +120,19 @@ $(function () {
                     $('#username').addClass('border border-1 border border-danger');
                     $('#submitButton').addClass('disabled');
                     $('#message').text('帳號已存在，請重新輸入');
+                    $('.erUsername').text('帳號已存在，請重新輸入');
                     
                 } else {
                     $('#username').removeClass('border border-1 border border-danger');
                     $('#submitButton').removeClass('disabled');
                     $('#message').text('帳號可以使用');
+                    $('.erUsername').text('帳號可以使用');
                 }
             }
         });
     });
 
-    //前端密碼格式判斷
+    //前端密碼格式判斷 erPassword
     $('#password').blur(function (e) {
         e.preventDefault();
 
@@ -136,16 +143,18 @@ $(function () {
             $('#submitButton').addClass('disabled');
             $('#password').addClass('border border-1 border border-danger');
             $('#message').text('密碼格式錯誤');
+            $('.erPassword').text('密碼格式錯誤');
             return;
         } else {
             $('#submitButton').removeClass('disabled');
             $('#password').removeClass('border border-1 border border-danger');
             $('#message').empty();
             $('#confirmPassword').focus();
+            $('.erPassword').empty();
         }
     });
 
-    //前端確認密碼格式判斷
+    //前端確認密碼格式判斷  erConfirmPassword
     $('#confirmPassword').blur(function (e) {
         e.preventDefault();
 
@@ -156,15 +165,17 @@ $(function () {
             $('#submitButton').addClass('disabled');
             $('#confirmPassword').addClass('border border-1 border border-danger');
             $('#message').text('密碼不吻合');
+            $('.erConfirmPassword').text('密碼不吻合');
         } else {
             $('#submitButton').removeClass('disabled');
             $('#confirmPassword').removeClass('border border-1 border border-danger');
             $('#message').empty();
+            $('.erConfirmPassword').empty();
         }
     });
 
-
-    //提交按鈕註冊Ajax請求
+    
+    //提交按鈕註冊Ajax請求  erGender
     $('#submitButton').click(function (e) {
 
         e.preventDefault();
@@ -182,6 +193,16 @@ $(function () {
         } else {
             $('#message').empty();
         }
+        
+        let sex = $("[name='gender']:checked").val();
+        // console.log(gender);
+        if(sex == null){
+            $('.erGender').text('請選擇性別');
+            alert("請選擇性別");
+        }else{
+            $('.erGender').empty();
+        }
+
 
 
         //創建物件
