3496eff36dc13b128dcf6d2c1eaea2c25735f58a
Kang-Zih-Jin <tony25174715@gmail.com>
2021-10-31 15:18:43 +0800

    整合mapSearch頁面 自動產生與換頁按鈕完成

--- src/main/java/com/java017/tripblog/controller/MapSearchController.java
+++ src/main/java/com/java017/tripblog/controller/MapSearchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MapSearchController {
@@ -30,5 +31,49 @@ public class MapSearchController {
         System.out.println(list);
         return list;
     };
+//----------------------------------------------------------------------------------
+    //輸入搜尋吧查詢並送出第一頁
+    @ResponseBody
+    @GetMapping("/firstSearchOfPage")
+    public List<Article> firstSearchOfPage(@RequestParam String enterAddressName){
+
+        List<Article> messageList;
+        messageList = articleService.getPagedArticles(0,5,enterAddressName);
+        System.out.println("messageList"+messageList);
+        return messageList;
+    }
+
+    //自動生成換頁按鈕
+    @ResponseBody
+    @GetMapping("/newPageButton")
+    public Integer findByAddressPage(@RequestParam String enterAddressName) {
+
+        //   Article article = articleRepository.findByEnterAddress(enterAddress);
+        ArrayList<Article> list;
+        list = articleService.findByEnterAddressNameLike(enterAddressName);
+        System.out.println("分頁按鈕"+list);
+        double listSize = list.size();
+        int pageMount = (int) Math.ceil(listSize/5);
+        return pageMount;
+    };
+
+    @ResponseBody
+    @GetMapping("/changeSearchOfPage")
+    public List<Article> changeSearchOfPage(@RequestParam String enterAddress,@RequestParam int page){
+
+        List<Article> messageList;
+        messageList = articleService.getPagedArticles(page,5,enterAddress);
+        System.out.println(messageList);
+        return messageList;
+
+    }
+
+
+
+
+
+
+
+
 }
 
