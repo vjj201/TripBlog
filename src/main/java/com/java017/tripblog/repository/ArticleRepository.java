e602b325072f77877a0a98ffbc7030df13395c8b
Kang-Zih-Jin <tony25174715@gmail.com>
2021-11-15 22:56:22 +0800

    修改import bug

--- src/main/java/com/java017/tripblog/repository/ArticleRepository.java
+++ src/main/java/com/java017/tripblog/repository/ArticleRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Optional;
 
