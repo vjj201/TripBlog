5642d692a846c2a398d4c0e69a74fa9b52da232e
samuela <Samuelahsu@gmail.com>
2021-11-21 21:19:48 +0800

    更新前端畫面ok

---
+++ src/main/java/com/java017/tripblog/service/FreeTagService.java
@@ -0,0 +1,10 @@
+package com.java017.tripblog.service;
+
+import com.java017.tripblog.entity.FreeTag;
+
+import java.util.ArrayList;
+
+
+public interface FreeTagService {
+    public ArrayList<FreeTag> findAllTags();
+}
